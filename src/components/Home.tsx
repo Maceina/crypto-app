5dd8fcd60bf1465cc9b4d2b0ce07f9b559915b56
Maceina <maceinaaudrius@gmail.com>
2021-06-19 19:18:36 +0300

    need to fix imports, because changed all cmp to cmp folder

---
+++ src/components/Home.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            <ul>
+                <li>a</li>
+                <li>a</li>
+                <li>a</li>
+                <li>a</li>
+                <li>a</li>
+                <li>a</li>
+                <li>a</li>
+                <li>a</li>
+                <li>a</li>
+                <li>a</li>
+                <li>a</li>
+                <li>a</li>
+                <li>a</li>
+                <li>a</li>
+                
+            </ul>
+        </div>
+    )
+}
+
+export default Home
